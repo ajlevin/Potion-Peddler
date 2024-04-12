9e50bc7940415691a1223480d8b8ad7d18d3dff4
ajlevin <ase.levine@gmail.com>
2024-04-12 13:03:21 -0700

    damn audit

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -11,33 +11,38 @@ def get_catalog():
     Each unique item combination must have only a single price.
     """
 
+    lst = []
+
     with db.engine.begin() as connection:
         amntRPotions = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory")).first()[0]
         amntGPotions = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).first()[0]
         amntBPotions = connection.execute(sqlalchemy.text("SELECT num_blue_potions FROM global_inventory")).first()[0]
 
-    print(amntRPotions, amntGPotions, amntBPotions)
-    
-    return [
-            {
+    if amntRPotions > 0:
+        lst.append({
                 "sku": "RED_POTION_1",
                 "name": "red potion",
                 "quantity": amntRPotions,
                 "price": 45,
                 "potion_type": [100, 0, 0, 0],
-            },
-            {
+            })
+    if amntGPotions > 0:
+        lst.append({
                 "sku": "GREEN_POTION_1",
                 "name": "green potion",
                 "quantity": amntGPotions,
                 "price": 45,
                 "potion_type": [0, 100, 0, 0],
-            },
-            {
+            })
+    if amntBPotions > 0:
+        lst.append({
                 "sku": "BLUE_POTION_1",
                 "name": "blue potion",
                 "quantity": amntBPotions,
                 "price": 45,
                 "potion_type": [0, 0, 100, 0],
-            }
-        ]
+            })
+
+    print(amntRPotions, amntGPotions, amntBPotions)
+    
+    return lst
