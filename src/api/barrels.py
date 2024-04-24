0658674c2ed00a4fd4d21f2e370400fcfe7f095d
ajlevin <ase.levine@gmail.com>
2024-04-23 22:56:54 -0700

    i am silly

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -31,6 +31,7 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
         dark_ml = 0
 
         for barrel_delivered in barrels_delivered:
+            gold_paid += barrel_delivered.price * barrel_delivered.quantity
             if barrel_delivered.potion_type == [1,0,0,0]:
                 red_ml += barrel_delivered.ml_per_barrel * barrel_delivered.quantity
             elif barrel_delivered.potion_type == [0,1,0,0]:
@@ -54,7 +55,7 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
                 num_dark_ml = num_dark_ml + :dark_ml, 
                 gold = gold - :gold_paid
                 """), 
-            [{"red_ml": red_ml, "green_ml": green_ml, "blue_ml": blue_ml, "dark_ml": dark_ml}])
+            [{"red_ml": red_ml, "green_ml": green_ml, "blue_ml": blue_ml, "dark_ml": dark_ml, "gold_paid": gold_paid}])
     
     return "OK"
 
